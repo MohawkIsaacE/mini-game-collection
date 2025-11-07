4b09a3f959565c9c0d16bc7def3a5c072f439041
LostPhantom18 <45826132+LostPhantom18@users.noreply.github.com>
2025-11-07 01:58:01 -0500

    Fixed Errors, Updated Team 09 Project to look correct.
    We had an error where event system and arcade input wasnt setup correctly. It has been formatted to look like Raphs game and now everything works.

---
+++ unity-project/mini-game-collection/Assets/TheControlScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MiniGameCollection.Games2025.Team09
+{
+    public class TheControlScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Score
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
